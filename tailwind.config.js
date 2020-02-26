4590524ee31fd5d050dbe36ea09443d75e8d1c90
Andrew <andrewbogdanov@ukr.net>
2020-02-26 21:48:16 +0300

    adding tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  theme: {
+    colors: {
+      active: 'var(--color-active)',
+      grey: 'var(--color-grey)',
+      white: 'var(--color-white)',
+      black: 'var(--color-black)',
+      pale: 'var(--color-pale)'
+    },
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif'],
+      merriweather: ['Merriweather Sans', 'sans-serif']
+    },
+    extend: {
+      screens: {
+        xxl: '1580px'
+      },
+      minHeight: {
+        xs: '48px',
+        sm: '80px'
+      },
+      borderRadius: {
+        xl: '24px'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
